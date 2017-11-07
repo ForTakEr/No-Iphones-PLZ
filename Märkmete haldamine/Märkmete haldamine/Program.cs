1ed46c332a79c25448a4dc0df1e1f44350020ba1
ForTakEr <32396272+ForTakEr@users.noreply.github.com>
2017-11-07 09:46:07 +0200

    Hetkel viimane push

--- Märkmete haldamine/Märkmete haldamine/Program.cs
+++ Märkmete haldamine/Märkmete haldamine/Program.cs
@@ -18,7 +18,11 @@ namespace Märkmete_haldamine
 
             if (cmd == "new")
             {
-
+                
+                New new1 = new New();
+                new1.FailiNimi = Console.ReadLine();
+                Console.WriteLine("Palun lisage märkus");
+                new1.Märkus = Console.ReadLine();
             }
 
             if (cmd == "read")
