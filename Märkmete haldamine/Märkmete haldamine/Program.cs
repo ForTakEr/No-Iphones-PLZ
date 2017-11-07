bf41e24514ed82869d08bd0a3d3d40e0d1c7dbd1
ForTakEr <32396272+ForTakEr@users.noreply.github.com>
2017-11-07 08:57:28 +0200

    Tegin muudatusi main failile

--- Märkmete haldamine/Märkmete haldamine/Program.cs
+++ Märkmete haldamine/Märkmete haldamine/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Märkmete_haldamine
 {
@@ -11,7 +12,36 @@ namespace Märkmete_haldamine
         static void Main(string[] args)
         {
             //IM ALIVE
+            Console.WriteLine("Palun valige, mida teha soovite");
+            Console.WriteLine("Commandid on: new; read; delete; exit");
+            string cmd = Console.ReadLine();
 
+            if (cmd == "new")
+            {
+
+            }
+
+            if (cmd == "read")
+            {
+
+            }
+
+            if (cmd == "delete")
+            {
+
+            }
+
+            else
+            {
+                Console.WriteLine("Commandid on: new; read; delete");
+            }
+
+            if (cmd == "exit")
+            {
+                Environment.Exit(0);
+            }
+
+            Console.ReadLine();
         }
     }
 }
