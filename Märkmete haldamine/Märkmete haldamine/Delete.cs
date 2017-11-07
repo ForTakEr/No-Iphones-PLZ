e54e5199beb36db58294b9287ef24020d6d01054
Kert Moorbach <33248196+KertM@users.noreply.github.com>
2017-11-07 09:28:50 +0200

    delete töötab nüüd
    aint nobody got time for that

--- Märkmete haldamine/Märkmete haldamine/Delete.cs
+++ Märkmete haldamine/Märkmete haldamine/Delete.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//4. Kasutaja saab märkmeid kustutada käsuga Delete -Failinimi-
 
 namespace Märkmete_haldamine
 {
     class Delete
     {
-    }
+        public Delete()
+        {
+            string input = Console.ReadLine();
+
+            if (input == (File.Exist))
+            {
+
+                File.Delete(input);
+            }
+            else
+            {
+                Console.WriteLine("sisestatu on vale");
+            }
+
+        }
+        
+
+
+    }      
+    
 }
