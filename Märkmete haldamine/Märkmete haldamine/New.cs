d06f9209e047604531a9b330c5f33cede8cc79b4
Gert-Andry <gertandry@gmail.com>
2017-11-07 09:32:56 +0200

    "New" klass on tehtud

--- Märkmete haldamine/Märkmete haldamine/New.cs
+++ Märkmete haldamine/Märkmete haldamine/New.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Märkmete_haldamine
 {
     class New
     {
+        public string path = Directory.GetCurrentDirectory();
+        public string FailiNimi { get; set; }
+        public string Märkus { get; set; }
+        public void New()
+        {
+            // Create a file to write to.
+            using (StreamWriter sw = File.CreateText(Path.Combine(path, FailiNimi + ".txt")))
+            {
+                sw.WriteLine(Märkus);
+            }
+        }
     }
 }
